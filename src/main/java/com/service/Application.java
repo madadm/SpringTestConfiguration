64977c3d4ff5f819e2080f6bbc9b533edc1d309b
madadm <lexi4.koval@gmail.com>
2019-04-04 21:18:27 -0400

    Multiple bindings fix

--- src/main/java/com/service/Application.java
+++ src/main/java/com/service/Application.java
@@ -12,11 +12,13 @@ public class Application {
     @Inject@Named("HelloWorldService")
     iGreetingService greeting = null;
 
+
     public Application(){}
     public Application(iGreetingService greeting){
         this.greeting = greeting;
     }
     public static void main(String[] args) {
+
         ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
         Application app = (Application)ctx.getBean("App");
         app.start();
